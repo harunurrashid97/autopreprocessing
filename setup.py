47a2b109221cb6fb037326c50cf79e692619945b
Harun-Ur-Rashid <harunspeedy1995@gmail.com>
2020-03-24 23:21:53 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'autodatapreprocessing',
+  packages = ['autodatapreprocessing'],
+  version = '0.1',
+  license='MIT',
+  description = 'Easy way to data preprocessing in python for machine learning and data science!',
+  author = 'Harun-Ur-Rashid',
+  author_email = 'harunspeedy1995@gmail.com',
+  url = 'https://github.com/harunshimanto/autodatapreprocessing',
+  download_url = 'https://github.com/harunshimanto/autodatapreprocessing/archive/v_01.tar.gz',
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'IPython',
+      ],
+  Meta =[
+  "Maintainer :: Quantum.ai Community",
+  ],
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering",
+  ],
+)
