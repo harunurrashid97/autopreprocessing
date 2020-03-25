de888d67a9a0f16d634c1a6033d88c7589e5aa60
Harun-Ur-Rashid <harunspeedy1995@gmail.com>
2020-03-25 10:02:32 +0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'autodatapreprocessing',
-  packages = ['autodatapreprocessing'],
-  version = '0.1',
-  license='MIT',
+  name = 'autopreprocessing',
+  packages = ['autopreprocessing'],
+  version = '1.0.01',
+  license='BSD 3-Clause License',
   description = 'Easy way to data preprocessing in python for machine learning and data science!',
   author = 'Harun-Ur-Rashid',
   author_email = 'harunspeedy1995@gmail.com',
   url = 'https://github.com/harunshimanto/autodatapreprocessing',
-  download_url = 'https://github.com/harunshimanto/autodatapreprocessing/archive/v_01.tar.gz',
+  download_url = 'https://github.com/harunshimanto/autodatapreprocessing/archive/v_1.0.01.tar.gz',
   classifiers=[
     "Development Status :: 5 - Production/Stable",
     "Environment :: Console",
